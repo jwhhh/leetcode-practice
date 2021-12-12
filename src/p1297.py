abfc3760ccd77f656d4f30572cd4e127edecfad5
Jiawen He <hanghur@gmail.com>
2021-12-12 13:09:57 +1100

    #1297 Maximum Number of Occurrences of a Substring (Python)

--- src/p1297.py
+++ src/p1297.py
@@ -1,22 +1,24 @@
-# Given a string s, return the maximum number of ocurrences of any substring under the following rules:
-
-#     The number of unique characters in the substring must be less than or equal to maxLetters.
-#     The substring size must be between minSize and maxSize inclusive.
+from collections import deque
 
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
         stats = dict()
         max_stats = 0
         len_s = len(s)
+        if len_s < minSize: 
+            return 0
+
+        sub_chars = deque(list(s[:minSize]))
         for i in range(len_s-minSize+1):
-            for l in range(minSize, maxSize+1):
-                j = i + l   # the j-th is not included
-                if j > len_s: break
-                substring = s[i:j]
-                if len(set(list(substring))) <= maxLetters:
-                    if substring not in stats:
-                        stats[substring] = 0
-                    stats[substring] += 1
-                    if stats[substring] > max_stats:
-                        max_stats = stats[substring]
+            j = i + minSize   # the j-th is not included
+            if i != 0:
+                sub_chars.append(s[j-1])
+                sub_chars.popleft()
+            if len(set(sub_chars)) <= maxLetters:
+                sub_string = ''.join(sub_chars)
+                if sub_string not in stats:
+                    stats[sub_string] = 0
+                stats[sub_string] += 1
+                if stats[sub_string] > max_stats:
+                    max_stats = stats[sub_string]
         return max_stats
